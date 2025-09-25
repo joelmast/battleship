bb162237ab0fff3c24b7b31c93b86e965ee54e13
Hamburger-coder <joesmast@gmail.com>
2025-09-25 17:50:51 -0400

    Added a prettier file for customization, completed hit() functionality and isSunked functionality

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 // For creating ship objects
 class Ship {
-  constructor(length, hits, isSunk) {
-    this.length = length;
-    this.hits = hits;
-    this.isSunk = isSunk;
-  }
+	constructor(length, hits, isSunk) {
+		this.length = length;
+		this.hits = hits;
+		this.isSunk = isSunk;
+	}
+	hit() {
+		this.hits++;
+		return this.hits;
+	}
+	isSunked() {
+		console.log("the function isSunked is running!");
+		console.log(this);
+		if (this.hits >= this.length) {
+			console.log("The first part of the if statement was triggered!");
+			this.isSunk = true;
+			return true;
+		} else {
+			console.log("The second part of the if statement was triggered!");
+			this.isSunk = false;
+			return false;
+		}
+	}
 }
 
 let ship = new Ship(5, 3, false);
-
-console.log(ship);
 
 module.exports = { Ship };
--- script.test.js
+++ script.test.js
@@ -1,13 +1,33 @@
 const { Ship } = require("./script.js");
 
 describe("Ship", () => {
-  let myShip;
+	let myShip;
+	let myShip2;
+	beforeEach(() => {
+		myShip = new Ship(5, 3, false);
+		myShip2 = new Ship(4, 4, true);
+	});
 
-  beforeEach(() => {
-    myShip = new Ship(5, 3, false);
-  });
+	test("ship test 1", () => {
+		expect(myShip).toEqual({ length: 5, hits: 3, isSunk: false });
+	});
+	test("ship test 2", () => {
+		expect(myShip2).toEqual({ length: 4, hits: 4, isSunk: true });
+	});
 
-  test("ship test 1", () => {
-    expect(myShip).toEqual({ length: 5, hits: 3, isSunk: false });
-  });
+	test("ship hit test 1", () => {
+		expect(myShip.hit()).toEqual(4);
+	});
+
+	test("ship hit test 2", () => {
+		expect(myShip2.hit()).toEqual(5);
+	});
+
+	test("ship isSunked test 1", () => {
+		expect(myShip.isSunked()).toEqual(false);
+	});
+
+	test("ship isSunked test 2", () => {
+		expect(myShip2.isSunked()).toEqual(true);
+	});
 });
